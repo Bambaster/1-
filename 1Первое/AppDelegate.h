87715b033f32c927c65bffe32f7f480c4d69f163
Vladimir <popovvs86@gmail.com>
2015-02-13 13:02:07 +0300

    Initial Commit

---
+++ 1Первое/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  1Первое
+//
+//  Created by Lowtrack on 13.02.15.
+//  Copyright (c) 2015 Vladimir Popov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 1Первое/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  1Первое
+//
+//  Created by Lowtrack on 13.02.15.
+//  Copyright (c) 2015 Vladimir Popov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
